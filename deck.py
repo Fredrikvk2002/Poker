edc85186f073eedd595753baeb559ea77edef65b
Fredrikvk2002 <fviksekarlse.ieu2023@student.ie.edu>
2025-03-28 12:22:31 +0100

    Finished Poker Game

--- deck.py
+++ deck.py
@@ -1,62 +1,55 @@
 import random
 
-
 class Card:
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["♣", "♦", "♥", "♠"]
-    # Clubs = [Clubs, diamonds, hearts, spades]
-
-
-
-    def __init__(self, rank, suit):
+    def __init__(self, suit, rank):
         if rank not in self.RANKS:
             raise ValueError("Invalid rank")
-        self._rank = rank
+        if suit not in self.SUITS:
+            raise ValueError("Invalid suit")
         self._suit = suit
-    @property
-    def rank(self):
-       return self._rank
+        self._rank = rank
+
+    def __eq__(self, other):
+        return self.rank == other.rank
+
+    def __gt__(self, other):
+        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
+
+    def __str__(self):
+        return f"{self._rank}{self._suit}"
+
+    def __repr__(self):
+        return self.__str__()
 
     @property
     def suit(self):
-       return self._suit
+        return self._suit
 
-
-    def __str__(self):
-       return f"{self._rank}{self._suit}"
-
-    def __repr__(self):
-        return self.__str__() # repr is the same as str
+    @property
+    def rank(self):
+        return self._rank
 
 class Deck:
     def __init__(self):
-        _cards = []
+        self._deck = []
         for suit in Card.SUITS:
             for rank in Card.RANKS:
-                _cards.append(Card(rank, suit))
-        self._cards = _cards
-
-    @property
-    def cards(self):
-        return self._cards
+                self._deck.append(Card(suit, rank))
 
     def __str__(self):
-        return str(self._cards)
+        return str(self._deck)
 
     def shuffle(self):
-       random.shuffle(self.cards)
+        random.shuffle(self._deck)
 
     def deal(self):
-        return self.cards.pop(0)
+        return self._deck.pop(0)
 
-
-
-
-c1 = Card("A", "♣")
-print(c1.suit, c1.rank)
-deck = Deck()
-print(deck)
-deck.shuffle()
-print(deck)
-print(deck.deal())
-print(deck)
+if __name__ == "__main__":
+    deck = Deck()
+    print(deck)
+    deck.shuffle()
+    print(deck)
+    print(deck.deal())
